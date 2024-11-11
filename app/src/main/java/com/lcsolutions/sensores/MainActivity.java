0e5078bfc14f151d70ee10c016cf990bd355461a
Luis <luisangel.cruz@upaep.edu.mx>
2024-11-11 10:52:54 -0600

    sensores 1

---
+++ app/src/main/java/com/lcsolutions/sensores/MainActivity.java
@@ -0,0 +1,30 @@
+package com.lcsolutions.sensores;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView txtSensorList = findViewById(R.id.txtSensorList);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor: sensorList
+             ) {
+         txtSensorList.append("\n"+sensor.getName());
+        }
+    }
+}
